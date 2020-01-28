0e2cd7bbd2b98c9d82fcfa0abf52977c44242494
kiran <kiraneng.kiran@gmail.com>
2020-01-28 19:29:14 +0530

    1st

---
+++ src/test/java/project16/SwapNumberstest.java
@@ -0,0 +1,19 @@
+package project16;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class SwapNumberstest {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
